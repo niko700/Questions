18d58db8ffe8832f736fb91a406d856591eef219
Vera Neroni <vnneroni@gmail.com>
2023-02-08 21:27:01 -0500

    error in previous submission resolved

---
+++ sumv9resolved.py
@@ -0,0 +1,25 @@
+#function that adds up numbers hidden in strings to one sum
+#multiple-digit numbers have to be preserved
+
+def grab_number_sum(word):
+    numbers = '1234567890'
+    found_numbers = []
+    counter = 0
+    r = len(word)
+
+    for i in range(r):
+        if i == 0:
+            if word[i] in numbers:
+                found_numbers.append(word[i])
+        else:
+            if (word[i] in numbers) and (word[i-1] not in numbers) and (found_numbers == []):
+                found_numbers.append(word[i])
+            elif (word[i] in numbers) and (word[i-1] not in numbers) and (found_numbers != []):
+                found_numbers.append(word[i])
+                counter += 1
+            elif (word[i] in numbers) and (word[i-1] in numbers):
+                found_numbers[counter] += word[i]
+
+    print(sum(map(int, found_numbers)))
+
+
