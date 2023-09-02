61526bb8940a29477b792a31db96c2b15558c9f1
niko700 <96093486+niko700@users.noreply.github.com>
2023-09-01 20:52:22 -0400

    Add files via upload

---
+++ tkinter percent calculator.py
@@ -0,0 +1,58 @@
+#This code is to create a small Tkinter window that asks for a number and
+#calculates what percentage it is of a larger number (hardcoded in).
+
+from sys import argv
+import tkinter as tk
+
+
+#making tkinter window
+window = tk.Tk()
+
+###make frames inside window
+###what do I do with these....?
+##frame_a = tk.Frame()
+##frame_b = tk.Frame()
+
+
+#make greeting line
+lbl_greeting = tk.Label(text = "What is your number?",
+                    width = 20,
+                    height = 5)
+lbl_greeting.pack()
+
+
+#making entry box
+ent_entry = tk.Entry()
+ent_entry.pack()
+
+#what to do with entered text
+string_number = entry.get()
+
+
+#making an "OK" button
+btn_button = tk.Button(text = "OK",
+                    width = 5,
+                    height = 1)
+btn_button.pack()
+
+#define calculate restuls function
+def convert(s):
+    n = int(s)
+    total = n/225*100
+    return print(str(round(total,2)))
+
+
+#button click event --> calculate what's in the box
+def handle_click(event):
+    convert(string_number)
+
+btn_button.bind("<Button-1>", handle_click)
+
+#make results text
+lbl_results = tk.Label(text = "You are " + output + " % done!",
+                    width = 20,
+                    height = 5)
+lbl_results.pack()
+
+#run the window
+window.mainloop()
