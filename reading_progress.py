d7e2f5e693a86c8fd42c9bf24a02aac4ecad5691
Vera Neroni <vnneroni@gmail.com>
2023-09-13 20:22:12 -0400

    string.set and out_string outside function

--- reading_progress.py
+++ reading_progress.py
@@ -1,8 +1,26 @@
-#This code is to create a small Tkinter window that asks for a number and
-#calculates what percentage it is of a larger number (hardcoded in).
+"""
+Date: 09/02/23
+Author: niko700
+Edits: 
+        - 09/13/23  - moved string variable into button click function
+                    - created second parameter, but gave it a default value because still testing input process
+                    - edited header
+                    - formatted questions as """ """ strings
 
-#Tried to incorporate some code from this example: https://insolor.github.io/effbot-tkinterbook-archive/entry.htm
-#Still get error where the entry window returns an empty string
+ Code Purpose:  
+
+ This code is to create a small Tkinter window that asks for a number and
+ calculates what percentage it is of a larger number (hardcoded in.
+ 
+ Tried to incorporate some code from this example: https://insolor.github.io/effbot-tkinterbook-archive/entry.htm
+
+ 
+Results:
+
+Still get error where the entry window returns an empty string.
+
+"""
+
 
 from sys import argv
 import tkinter as tk
@@ -26,8 +44,9 @@ string = tk.StringVar()
 ent = tk.Entry(textvariable=string)
 ent.pack()
 
-string.set("")
-out_string = string.get() #do I keep this? or is this made obsolete by the btn.out_string inside the function? also, switch this places with the entry widget, so it's closer to the declaration?
+# string.set("")
+# out_string = string.get() 
+"""do I keep this (above)? or is this made obsolete by the btn.out_string inside the function? also, switch this places with the entry widget, so it's closer to the declaration?"""
 
 
 #making an "OK" button
@@ -44,19 +63,30 @@ lbl = tk.Label(text = "",
 lbl.pack()
 
 
-#define calculate restuls function
-def convert(s):
-    n = int(s)
-    total = n/225*100
-    return print(str(round(total,2)))
+#define calculate results function
+def convert(s1, s2="225"):
+    n1 = int(s1)
+    n2 = int(s2)
+    total = n1/n2*100
+    string_total = str(round(total,2))
+    print(string_total) #to check while running
+    print(type(string_total))
+    return string_total
 
 
 #button click event --> calculate what's in the box
 def handle_click(event):
-    btn.out_string = string.get()  #wouldn't this have to be reversed? because you are trying to get the variable to equal to the output of the button, not the other way around?
+    btn.out_string = string.get()   
+    """so, this is creating an instance variable here, with btn.out_string being the object?"""
+    """also, this equals expression does not need to be reversed? if because you are trying to get the variable to equal to the output of the button, not the other way around?"""
+    
     print("You typed: " + btn.out_string)
     
-    total = convert(out_string)
+    #calculate percent using convert() function
+    total = convert(btn.out_string)
+    print(type(total))
+    
+    #create display text using result above
     to_display = "You're " + total + " % done!"
     lbl.configure(text = to_display)
 
